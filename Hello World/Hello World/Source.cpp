dabedc6756ebdef0ef625036916da6674bc676ae
Francesc Ripoll <fripoll.santmiquel@gmail.com>
2016-02-16 23:57:10 +0100

    Hello World Frun

--- Hello World/Hello World/Source.cpp
+++ Hello World/Hello World/Source.cpp
@@ -6,9 +6,11 @@ int main()
 
 	printf("Hello World -> Simon Stoyanov\n");
 
-	printf("Hello World -> Guillem Sunyer\n");
+	printf("Hello World -> Guillem Sunyer -> UltraDickRider\n");
 
-	printf("Hello World -> Arnau Junoy\n");
+	printf("Hello World -> Arnau Junoy -> UltraRider\n");
+	
+	printf("Hello World -> Francesc Ripoll\n");
 
 	system("pause");
 	return 0;
